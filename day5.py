f49b3901fea1feea304789a4d35d318a6aa7028a
dimcaputo <dim.caputo@gmail.com>
2024-12-06 14:00:28 +0100

    Works now, the issue was with the position of the middle of the list, the code was simplified although it was working beforehand

--- day5.py
+++ day5.py
@@ -5,7 +5,7 @@ try:
         for line in f.readlines():
             stripped = line.strip('\n')
             if '|' in stripped:
-                order_pages.append([stripped.split('|')[0]],stripped.split('|')[1])
+                order_pages.append((stripped.split('|')[0],stripped.split('|')[1]))
             else: 
                 to_print.append(stripped.split(','))
 except:
@@ -15,7 +15,7 @@ except:
         for line in f.readlines():
             stripped = line.strip('\n')
             if '|' in stripped:
-                order_pages.append(([stripped.split('|')[0]],stripped.split('|')[1]))
+                order_pages.append((stripped.split('|')[0],stripped.split('|')[1]))
             else: 
                 to_print.append(stripped.split(','))
 
@@ -23,43 +23,20 @@ to_print.remove([''])
 okay = []
 sum_middle = 0
 
-def in_tuple_left(page):
-    verif = False
-    for tup in order_pages:
-        if page == tup[0]:
-            verif = True
-    return verif
+def check_updates(lst_lst, lst_tup):
+    for lst in lst_lst:
+        is_okay = True
+        for i in range(len(lst)-1):
+            if (lst[i+1], lst[i]) in lst_tup:
+                is_okay = False
+        if is_okay == True:
+            okay.append(lst)
 
-def in_tuple_right(page):
-    verif = False
-    for tup in order_pages:
-        if page == tup[1]:
-            verif = True
-    return verif
-
-def get_left(page):
-    lst = []
-    for tup in order_pages:
-        if page == tup[1]:
-            lst.append(tup[0])
-    return lst
-
-def check_updates(lst_lst, dico):
-    for line in lst_lst:
-        lst_temp = line.copy()
-        for item in line[::-1]:
-            if not in_tuple_left(item) and not in_tuple_right(item):
-                lst_temp.remove(item)
-            elif in_tuple_right(item) and all([x not in lst_temp for x in get_left(item)]):   #Not good here
-                lst_temp.remove(item)
-        if lst_temp == []:
-            okay.append(line)
-
-check_updates(to_print, order_pages)
+check_updates(to_print, order_pages)            
 
 for lst in okay:
     if len(lst)%2 != 0:
-        mid = int(len(lst)/2)+1
+        mid = int(len(lst)/2)
         sum_middle += int(lst[mid])
 
 print(sum_middle)
